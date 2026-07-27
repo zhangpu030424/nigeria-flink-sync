bfa3bf2a30c7fc2d42451c112333a07d670e4e30
zhangpu <18530307681@163.com>
2026-07-27 16:24:20 +0800

    Speed up user_info bundle lookup: index-friendly adid join, drop unused columns.

--- sql/ddl/source_lookup_views.sql
+++ sql/ddl/source_lookup_views.sql
@@ -321,66 +321,6 @@ SELECT u.id AS user_id,
        CAST(ac.version AS CHAR) AS app_version,
        CAST(cc.credit_limit AS CHAR) AS credit_limit,
        CAST(dn.ip AS CHAR) AS reg_ip,
-       CAST((
-                 SELECT COALESCE(
-                                JSON_ARRAYAGG(
-                                        JSON_OBJECT(
-                                                'name', NULLIF(TRIM(ec.contact_name), ''),
-                                                'mobile', CASE
-                                                              WHEN ec.contact_number IS NULL OR TRIM(ec.contact_number) = ''
-                                                                  THEN CAST(NULL AS JSON)
-                                                              WHEN vt5.token IS NOT NULL AND TRIM(vt5.token) <> ''
-                                                                  THEN vt5.token
-                                                              ELSE (
-                                                                  CASE
-                                                                      WHEN TRIM(ec.contact_number) LIKE '+%'
-                                                                          THEN TRIM(ec.contact_number)
-                                                                      WHEN TRIM(ec.contact_number) LIKE '234%'
-                                                                          THEN CONCAT('+', TRIM(ec.contact_number))
-                                                                      WHEN TRIM(ec.contact_number) LIKE '0%'
-                                                                          THEN CONCAT('+234', SUBSTRING(TRIM(ec.contact_number), 2))
-                                                                      ELSE CONCAT('+234', TRIM(ec.contact_number))
-                                                                      END
-                                                                  )
-                                                    END,
-                                                'relation', ec.contact_relationship
-                                        )
-                                ),
-                                JSON_ARRAY()
-                        )
-                 FROM user_emergency_contact ec
-                          LEFT JOIN vt_token_cache vt5
-                                    ON vt5.vt_type = 5 AND vt5.status = 1
-                                        AND vt5.token IS NOT NULL AND TRIM(vt5.token) <> ''
-                                        AND vt5.raw_value COLLATE utf8mb4_bin = (
-                                            CASE
-                                                WHEN ec.contact_number IS NULL OR TRIM(ec.contact_number) = '' THEN NULL
-                                                WHEN TRIM(ec.contact_number) LIKE '+%' THEN TRIM(ec.contact_number)
-                                                WHEN TRIM(ec.contact_number) LIKE '234%'
-                                                    THEN CONCAT('+', TRIM(ec.contact_number))
-                                                WHEN TRIM(ec.contact_number) LIKE '0%'
-                                                    THEN CONCAT('+234', SUBSTRING(TRIM(ec.contact_number), 2))
-                                                ELSE CONCAT('+234', TRIM(ec.contact_number))
-                                                END
-                                            ) COLLATE utf8mb4_bin
-                 WHERE ec.user_id = u.id
-             ) AS CHAR) AS emergency_contacts,
-       CAST(
-               CASE
-                   WHEN adj.tracker_name IS NULL OR TRIM(adj.tracker_name) = '' THEN NULL
-                   WHEN LOWER(TRIM(adj.tracker_name)) LIKE '%unattributed%' THEN NULL
-                   WHEN LOWER(TRIM(adj.tracker_name)) LIKE '%organic%' THEN 'ORGANIC'
-                   WHEN LOWER(TRIM(adj.tracker_name)) LIKE '%google%' THEN 'GG'
-                   WHEN LOWER(TRIM(adj.tracker_name)) LIKE '%apple%' THEN 'ASA'
-                   WHEN LOWER(TRIM(adj.tracker_name)) LIKE '%tiktok%' THEN 'TT'
-                   WHEN LOWER(TRIM(adj.tracker_name)) LIKE '%facebook%'
-                       OR LOWER(TRIM(adj.tracker_name)) LIKE '%instagram%'
-                       OR LOWER(TRIM(adj.tracker_name)) LIKE '%messenger%' THEN 'FB'
-                   WHEN LOWER(TRIM(adj.tracker_name)) LIKE '%sms%' THEN 'SMS'
-                   WHEN LOWER(TRIM(adj.tracker_name)) LIKE '%kuai%' THEN 'KW'
-                   ELSE TRIM(adj.tracker_name)
-               END AS CHAR
-       ) AS install_source,
        CAST(JSON_OBJECT(
                'birthday', DATE_FORMAT(p.date_of_birth, '%Y-%m-%d'),
                'job_type', wr.work_type,
@@ -507,14 +447,14 @@ FROM `user` u
                        WHERE cc2.user_id = u.id
                    )
          LEFT JOIN adjust_latest_by_adid adj
-                   ON u.adid IS NOT NULL AND TRIM(u.adid) <> '' AND adj.adid = u.adid
+                   ON u.adid IS NOT NULL AND u.adid <> '' AND adj.adid = u.adid
          LEFT JOIN device_network dn
                    ON dn.id = (
                        SELECT MAX(dn2.id)
                        FROM device_network dn2
-                       WHERE u.device_id IS NOT NULL AND TRIM(u.device_id) <> ''
+                       WHERE u.device_id IS NOT NULL AND u.device_id <> ''
                          AND dn2.device_uuid = u.device_id
-                         AND dn2.ip IS NOT NULL AND TRIM(dn2.ip) <> ''
+                         AND dn2.ip IS NOT NULL AND dn2.ip <> ''
                    );
 
 -- ========== user / user_bankcard / user_product ==========
